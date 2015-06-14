59f81adfd1ade63c1d554080198d9a7734362fc4
Sebareth <vinet.sebastien-win10@outlook.com>
2015-06-14 15:13:53 +0200

    - Added QUEST1: Choose X seconds to shutdown - Added QUEST2: Confirm X seconds to shutdown - Added :PAUSE

--- Batch-stand/shutdownXseconds.bat
+++ Batch-stand/shutdownXseconds.bat
@@ -1,11 +1,34 @@
-@echo off
+@echo off & cls
+Mode con cols=72 lines=10
+title %title%
+
+set title=Shutdown in X seconds
+
+echo ***************************
+echo ** Shutdown in X seconds **
+echo ***************************
+
+:QUEST1
+echo.
+set /p seconds=Shutdown in ___ seconds ? :
+echo You choose to shutdown in %seconds% seconds !
+goto QUEST2
+
+:QUEST2
+set /P confirm=Confirm to shutdown in %seconds% seconds[Y/N]?
+if /I "%confirm%" EQU "Y" goto :SHUTDOWN
+if /I "%confirm%" EQU "N" goto :PAUSE
+goto QUEST1
+
+:SHUTDOWN
 cls
-title Shutdown in X seconds
-::title %title%
+title Shutdown in %seconds%
+SHUTDOWN /s /t %seconds% /c "Shutdown in progress, you have %seconds% seconds."
+goto EOF
 
-:: Add question: You choose to shutdown your computer, choose X seconds [Y/N]:
-:: set X=
-:: set title=Shutdown in X seconds
+:PAUSE
+pause>nul
+goto QUEST1
 
-SHUTDOWN /s /t 30 /c "Shutdown in progress, you have X seconds"
-pause
+:EOF
+EXIT
