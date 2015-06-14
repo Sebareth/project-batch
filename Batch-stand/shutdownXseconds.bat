87df9d5edbe5289d8702039e560810e6b0c1b210
Sebareth <vinet.sebastien-win10@outlook.com>
2015-06-14 02:17:26 +0200

    Added TODO

--- Batch-stand/shutdownXseconds.bat
+++ Batch-stand/shutdownXseconds.bat
@@ -1,6 +1,11 @@
 @echo off
 cls
-title Shutdown X seconds
+title Shutdown in X seconds
+::title %title%
+
+:: Add question: You choose to shutdown your computer, choose X seconds [Y/N]:
+:: set X=
+:: set title=Shutdown in X seconds
 
 SHUTDOWN /s /t 30 /c "Shutdown in progress, you have X seconds"
 pause
