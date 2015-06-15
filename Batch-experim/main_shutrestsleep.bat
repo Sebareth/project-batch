5cd779758936ba1a151cba8c27d46567194d1c53
Sebareth <vinet.sebastien-win10@outlook.com>
2015-06-15 18:58:32 +0200

    - added change SESSION - added not functionnal cancel shitdown /a

--- Batch-experim/main_shutrestsleep.bat
+++ Batch-experim/main_shutrestsleep.bat
@@ -15,7 +15,8 @@ Mode con cols=72 lines=10
 ::Refresh Explorer.exe
 ::
 :menuLOOP
-Set menu_text= Power option: Sleep, Shutdown, Restart, Refresh
+Set menu_text=Power option: Sleep, Shutdown, Restart, Session, Refresh
+Set cancel_text=To cancel any previous actions, type X
 Set option_text=Choose an option:
 @echo off & cls
 @title Batch Utility
@@ -24,6 +25,7 @@ echo.      ========================Menu============================
 echo		%menu_text% 
 echo.
 for /f "tokens=1,2,* delims=_ " %%A in ('"findstr /b /c:":menu_" "%~f0""') do echo.           %%B  %%C
+echo		%cancel_text%
 echo.      ========================================================
 Set option=
 echo. & set /p option=%option_text% || GOTO :EOF
@@ -31,9 +33,9 @@ echo. & call :menu_[%option%]
 pause
 
 ::*****************************************************************
-:menu_[1] Sleep in X seconds
+:menu_[1] Sleep
 cls
-title Sleep in X seconds
+title Sleep
 :SECONDS
 echo.
 Set /p seconds=Sleep in ___ seconds ? :
@@ -48,16 +50,16 @@ if /I "%confirm%" EQU "N" goto :menuLOOP
 :SLEEP
 cls
 title Sleep in %seconds%
-SHUTDOWN /h /t %seconds% /c "Sleep in progress, you have %seconds% seconds."
+SHUTDOWN -i /h /t %seconds% /c "Sleep in progress, you have %seconds% seconds."
 echo This script will be closed in few seconds.
 Ping 127.0.0.1 3>&1 >nul 2>&1
 ::1>nul timeout /t 3 /nobreak
 goto EOF
 
 ::*****************************************************************
-:menu_[2] Shutdown in X seconds
+:menu_[2] Shutdown
 cls
-title Shutdown in X seconds
+title Shutdown
 :SECONDS
 echo.
 Set /p seconds=Shutdown in ___ seconds ? :
@@ -72,16 +74,16 @@ if /I "%confirm%" EQU "N" goto :menuLOOP
 :SHUTDOWN
 cls
 title Shutdown in %seconds%
-SHUTDOWN /s /t %seconds% /c "Shutdown in progress, you have %seconds% seconds."
+SHUTDOWN -i /s /t %seconds% /c "Shutdown in progress, you have %seconds% seconds."
 echo This script will be closed in few seconds.
 Ping 127.0.0.1 3>&1 >nul 2>&1
 ::1>nul timeout /t 3 /nobreak
 goto EOF
 
 ::*****************************************************************
-:menu_[3] Restart in X seconds
+:menu_[3] Restart
 cls
-title Restart in X seconds
+title Restart
 :SECONDS
 echo.
 Set /p seconds=Restart in ___ seconds ? :
@@ -96,14 +98,37 @@ if /I "%confirm%" EQU "N" goto :menuLOOP
 :RESTART
 cls
 title Restart in %seconds%
-SHUTDOWN /r /t %seconds% /c "Restart in progress, you have %seconds% seconds."
+SHUTDOWN -i /r /t %seconds% /c "Restart in progress, you have %seconds% seconds."
 echo This script will be closed in few seconds.
 Ping 127.0.0.1 3>&1 >nul 2>&1
 ::1>nul timeout /t 3 /nobreak
 goto EOF
 
 ::*****************************************************************
-:menu_[4] Refresh your Desktop
+:menu_[4] Change current session
+cls
+title Change current session
+:SECONDS
+echo.
+Set /P seconds=Change current session in ___ seconds ? :
+echo You choose to change current session in %seconds% seconds !
+goto CONFIRM
+
+:CONFIRM
+Set /P confirm=Confirm to change current session in %seconds% seconds[Y/N]?
+if /I "%confirm%" EQU "Y" goto :SESSION
+if /I "%confirm%" EQU "N" goto :menuLOOP
+
+:SESSION
+cls
+title Change current session in %seconds%
+SHUTDOWN -i /l /f /t %seconds% /c "Change current session in progress, you have %seconds% seconds."
+echo This script will be closed in few seconds.
+Ping 127.0.0.1 3>&1 >nul 2>&1
+::1>nul timeout /t 3 /nobreak
+goto EOF
+::*****************************************************************
+:menu_[5] Refresh your Desktop
 set ExplorerExe=explorer.exe
 set Kill=You choose to restart your desktop, this will take few seconds.
 set Running=Success! Your desktop has been restarted.
@@ -119,20 +144,29 @@ goto :CHOICE1
 
 :REFRESH
 echo %Kill%
-ping -n 5 127.0.0.1 >NUL 2>&1   
-taskkill /F /IM %ExplorerExe% >NUL
+ping -n 5 127.0.0.1 >nul 2>&1   
+taskkill /F /IM %ExplorerExe% >nul
 echo.
-ping -n 10 127.0.0.1 >NUL 2>&1   
+ping -n 10 127.0.0.1 >nul 2>&1   
 echo.
-start %ExplorerExe% >NUL
+start %ExplorerExe% >nul
 echo %Running%
 1>nul timeout /t 20 /nobreak   
 echo.   
 echo %Fully%
-pause>NUL
+pause>nul
 goto:menuLOOP
 
 ::*****************************************************************
+:CANCEL
+echo %cancel_text%
+Set /P "=cancel=Type X to cancel:" > nul
+if /I "%cancel%" EQU "X" goto :CANCEL
+::SHUTDOWN /a
+echo %% have been cancelled.
+pause
+goto:menuLOOP
+
 :PAUSE
 pause>nul
 goto:menuLOOP
